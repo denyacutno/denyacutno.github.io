5cdd8e580174f2b788438ea6a6c2ea2192af3110
fayfayer <musafii.fai@outlook.com>
2019-03-08 10:43:07 +0700

    api list testing

---
+++ precache-manifest.8062b29e53b3b2a0572f77fa39d19f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b64ff84c2c885638acb",
+    "url": "/static/js/main.5b64ff84.chunk.js"
+  },
+  {
+    "revision": "73283069f212b057eefd",
+    "url": "/static/js/1.73283069.chunk.js"
+  },
+  {
+    "revision": "5b64ff84c2c885638acb",
+    "url": "/static/css/main.2a433f64.chunk.css"
+  },
+  {
+    "revision": "9da65f6f07f721d28d70380a3cdd2a4e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/denyacutno/denyacutno.github.io.git/precache-manifest.97add9c4c91b5985442575c970b3104d.js"
+  "/precache-manifest.8062b29e53b3b2a0572f77fa39d19f5a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/denyacutno/denyacutno.github.io.git/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
